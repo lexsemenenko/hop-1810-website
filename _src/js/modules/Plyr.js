4d3cf13db0acb2cca8d7dd08f1cb2780ac0f1e06
Lex Semenenko <lex@lex.me>
2019-08-06 00:22:57 -0400

    Sermon Video started

--- _src/js/modules/Plyr.js
+++ _src/js/modules/Plyr.js
@@ -1,4 +1,8 @@
 import Plyr from 'plyr';
-const plyrAduio = new Plyr('.plyr-player', {
+const plyrAduio = new Plyr('.plyr-audio-player', {
   autoplay: false
+});
+
+const plyrYoutube = new Plyr('.plyr__video-embed', {
+  showinfo: 0
 });
