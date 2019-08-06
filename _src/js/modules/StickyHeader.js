57fd176f056c991ab2761b5427c93f21bce92be1
Lex Semenenko <lex@lex.me>
2019-08-06 13:54:33 -0400

    Site header style edits

--- _src/js/modules/StickyHeader.js
+++ _src/js/modules/StickyHeader.js
@@ -2,13 +2,14 @@ import $ from 'jquery';
 import ScrollMagic from 'scrollmagic';
 import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 
-
-
 class StickyHeader {
   constructor() {
-    this.controller = new ScrollMagic.Controller();
     // this.headerHeight = $('.site-header').outerHeight();
+    this.createNewScrollMagic()
+  }
 
+  createNewScrollMagic() {
+    this.controller = new ScrollMagic.Controller();
     this.scene =  new ScrollMagic.Scene({
       triggerElement: '.section--content',
       duration: 0,
